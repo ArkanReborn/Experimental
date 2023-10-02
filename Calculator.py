5110bcf334026ff3be7249a4becf25b7593ff7cd
DarkPhoenixReborn <mason.kimball@icloud.com>
2023-10-02 17:10:57 -0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,39 @@
+import math
+# Calculator
+
+num1 = float(input("What would you like your first number to be? :\n"))
+num2 = float(input("What would you like your second number to be? :\n"))
+op = input("What operation would you like to use? : (+, -, /, *, ^, |, <, >)\n")
+
+solution = 0
+
+if op == "+":
+    solution = num1 + num2
+    print('The answer to your problem is:', solution)
+elif op == "-":
+    solution = num1 - num2
+    print('The answer to your problem is:', solution)
+elif op == "/":
+    solution = num1 / num2
+    print('The answer to your problem is:', solution)
+elif op == "*":
+    solution = num1 * num2
+    print('The answer to your problem is:', solution)
+elif op == "^":
+    solution = (math.pow((num1), num2))
+    print('The answer to your problem is:', solution)
+elif op == "|":
+    solution = (math.sqrt(num1))
+    print('The answer to your problem is:', solution)
+elif op == ">":
+    if num1 > num2:
+        print('The answer to your problem is: True')
+    else:
+        print("The answer to your problem is false")
+elif op == "<":
+    if num1 < num2:
+        print('The answer to your problem is: True')
+    else:
+        print("The answer to your problem is false")
+else:
+    print('Unsupported operation!')
