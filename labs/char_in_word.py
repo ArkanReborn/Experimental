2da68b25010b3e4aa67e0dfd8ee87a8015022fd3
DarkPhoenixReborn <mason.kimball@icloud.com>
2023-10-11 12:50:01 -0500

    2 labs

---
+++ labs/char_in_word.py
@@ -0,0 +1,35 @@
+uin1 = input()
+uin2 = input()
+
+ulen1 = len(uin1)
+ulen2 = len(uin2)
+
+checker = 0
+
+if ulen1 > ulen2:
+    for letter in range(ulen2):
+        if uin1[letter] == uin2[letter]:
+            checker += 1
+    if checker > 1 or checker == 0:
+        print(f'{checker} characters match')
+    else: 
+        print(f'{checker} character matches')
+            
+            
+elif ulen1 < ulen2:
+    for letter in range(ulen1):
+        if uin1[letter] == uin2[letter]:
+            checker += 1
+    if checker > 1 or checker == 0:
+        print(f'{checker} characters match')
+    else: 
+        print(f'{checker} character matches')
+
+elif ulen1 == ulen2:
+    for letter in range(ulen1):
+        if uin1[letter] == uin2[letter]:
+            checker += 1
+    if checker > 1 or checker == 0:
+        print(f'{checker} characters match')
+    else: 
+        print(f'{checker} character matches')
---
+++ labs/largest_num.py
@@ -0,0 +1,9 @@
+x = int(input())
+mn = x
+mx = x
+
+while x >= 0:
+    if mx < x:
+        mx = x
+    x = int(input())
+print(f'{mx}')
