50e55d412a7153c6315ea05128a43fc33b3446b5
DarkPhoenixReborn <mason.kimball@icloud.com>
2023-10-13 13:19:51 -0500

    Optimized wind-chill and added new labs

---
+++ labs/output_range.py
@@ -0,0 +1,10 @@
+num1 = int(input())
+num2 = int(input())
+
+if num1 > num2:
+    print('Second integer can\'t be less than the first.')
+    exit(0)
+
+for number in range(num1, num2 + 1, 5):
+    print(number, end=' ')
+print()        
