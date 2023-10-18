6eef88817dd9ea5c3587840898497ca2d346b9dc
ArkanReborn <69182771+ArkanReborn@users.noreply.github.com>
2023-10-18 11:55:59 -0500

    Update num.py

---
+++ labs/num.py
@@ -0,0 +1,15 @@
+low = int(input())
+high = int(input())
+
+x = int(input())
+
+counter = 0
+
+if low + x >= high:
+    print('0')
+    exit(0)
+
+while low < high:
+    counter += 1
+    low += x    
+print(counter)
