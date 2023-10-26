8af8fe8f784616dd03722eb4344695b081885c2c
DarkPhoenixReborn <mason.kimball@icloud.com>
2023-10-26 13:38:47 -0500

    Fixed some typos in the functional code

--- labs/wind-chill.py
+++ labs/wind-chill.py
@@ -3,15 +3,15 @@ import math
 temp = int(input("Enter current Farenheit temperature, must be value >= -58 and <= 50 degrees: "))
 ws = int(input("Enter the current wind speed that must be >= 3 mph: "))
 
-if temp < -58 or temp > 50:
+if (temp < -58 or temp > 50) and ws < 3:
+  print('No wind chill temperature calculated:\nInvalid temperature and Wind Speed Entered! Both outside of valid ranges')
+elif temp < -58 or temp > 50:
   print("No wind chill temperature calculated:\nInvalid Temperature: valid value must be >= -58 and <= 50 degrees")
 elif ws < 3:
-  print("No wind chill temperature calculated:\nInvalid Wind Speed: valid value, must be >= 3 mph")
-elif temp < -58 or temp > 50 and ws > 3:
-  print('No wind chill temperature calculated:\nInvalid temperature and Wind Speed Entered! Both outside of valid ranges')
+  print("No wind chill temperature calculated: \nInvalid Wind Speed: valid value must be >= 3 mph")
 else:
 
-  celsius = ((temp - 32) * 5) / 9
+  celsius = (temp - 32) * (5 / 9)
 
   print()
   print(f'The current Farenheit temperature is: {temp:.2f}')
