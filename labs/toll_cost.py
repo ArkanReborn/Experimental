954f7433f8f717a9bf3d6972f84372f0a7f3f87e
DarkPhoenixReborn <mason.kimball@icloud.com>
2023-11-01 22:29:44 -0500

    Nov 1 Labs

---
+++ labs/toll_cost.py
@@ -0,0 +1,29 @@
+def calc_toll(hour, is_morning, is_weekend):
+    
+    cost = 0.0
+    
+    if (hour < 7) and (is_morning == True):
+        cost = 1.15
+        if is_weekend == True:
+            cost -= 0.10
+    elif (hour >= 7 and hour < 10) and (is_morning == True):
+        cost = 2.95
+        if is_weekend == True and hour < 8:
+            cost -= 0.80
+    elif (hour >= 10 and is_morning == True) or (hour < 3 and is_morning == False):
+        cost = 1.90
+        if is_weekend == True:
+            cost += 0.25
+    elif (hour >= 3 and hour < 8) and (is_morning == False):
+        cost = 3.95
+    elif (hour >= 8 and is_morning == False):
+        cost = 1.40
+        if is_weekend == True:
+            cost -= 0.30
+    return round(cost, 2)
+    
+if __name__ == '__main__':
+    print(calc_toll(8, True, False))
+    print(calc_toll(1, False, False))
+    print(calc_toll(3, False, True))
+    print(calc_toll(5, True, True))
