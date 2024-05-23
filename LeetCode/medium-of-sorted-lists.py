30240819ab924f0bd7c39e5931a252e08b7dedb1
ArkanReborn <mason.kimball@icloud.com>
2024-05-23 15:53:28 -0500

    Final (hard) problem

--- LeetCode/medium-of-sorted-lists.py
+++ LeetCode/medium-of-sorted-lists.py
@@ -5,23 +5,7 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        mergedList = nums1 + nums2
-        mergedList.sort()
-        total = 0
-        if len(mergedList) > 1:
-            for i in mergedList:
-                print(i)
-                if (mergedList[i - 1] <= 0):
-                    del mergedList[i]
-
-            lstLen = len(mergedList)
-
-            for i in mergedList:
-                total += mergedList[i - 1]
-            print(total)
-            print(mergedList)
-            print(lstLen)
-        else:
-            return mergedList[0]
-        return total / float(lstLen)
-# Not finished yet. It is a work in progress
+        mergedList = sorted(nums1 + nums2)
+        mergedLen = len(mergedList)
+        midNum = mergedLen // 2
+        return (mergedList[midNum] + mergedList[midNum - 1]) / 2.0 if mergedLen % 2 == 0 else mergedList[midNum]
